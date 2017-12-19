d60f9099634e9db474fa2ed05588c954cc414ea1
shameng <879163994@qq.com>
2017-12-19 23:22:44 +0800

    初始化项目

---
+++ src/main/java/com/meng/hellofeign/HelloFeignApplication.java
@@ -0,0 +1,12 @@
+package com.meng.hellofeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFeignApplication.class, args);
+	}
+}
